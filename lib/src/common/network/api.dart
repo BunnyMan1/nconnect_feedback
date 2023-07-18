0cc594291771513d6e246788f8d77aa03adddbb6
Ravalika Madari <110080939+ravalikamadari07@users.noreply.github.com>
2023-07-18 17:44:15 +0530

    Update api.dart

--- lib/src/common/network/api.dart
+++ lib/src/common/network/api.dart
@@ -52,8 +52,8 @@ class NdashApi {
       }
 
       if (screenshot == null ||
-          response?.statusCode == 200 ||
-          response?.statusCode == 201) {
+          response!.statusCode == 200 ||
+          response.statusCode == 201) {
         // success 🎉'
         Dio dio2 = Dio();
         dio2.options.headers['authorization'] = 'Bearer ${feedback.token}';
@@ -73,11 +73,11 @@ class NdashApi {
           "physical_size": getCSV(feedback.deviceInfo.physicalSize),
           "attachments": [
             {
-              "id": response?.data[0]['id'],
-              "display_order": response?.data[0]['display_order'],
-              "display_label": response?.data[0]['display_label'],
-              "description": response?.data[0]['description'],
-              "is_primary": response?.data[0]['is_primary']
+              "id": response!.data[0]['id'],
+              "display_order": response.data[0]['display_order'],
+              "display_label": response.data[0]['display_label'],
+              "description": response.data[0]['description'],
+              "is_primary": response.data[0]['is_primary']
             }
           ]
         };
@@ -91,8 +91,8 @@ class NdashApi {
 
         throw NdashApiException(response: response);
       }
-      if (response?.statusCode == 401) {
-        throw UnauthenticatedNdashApiException(response!);
+      if (response.statusCode == 401) {
+        throw UnauthenticatedNdashApiException(response);
       }
 
       throw NdashApiException(response: response);
