3e73b4765cb6c9099ca1548ea2edce47f78733f4
ranadeepmedagone <ranadeep.medagone@narayanagroup.com>
2026-08-31 14:55:38 +0530

    flutter upgrade

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/nspiraappscbd/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/nspiraappscbd/Documents/GitHub/nconnect_feedback/example"
+export "FLUTTER_ROOT=/Users/ranadeepmedagone/fvm/versions/3.47.2"
+export "FLUTTER_APPLICATION_PATH=/Users/ranadeepmedagone/Documents/GitHub/nconnect_feedback/example"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=/Users/ranadeepmedagone/Documents/GitHub/nconnect_feedback/example/ios/Flutter/ephemeral/Packages/.packages/FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
